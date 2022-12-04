9f8c1e806b709e2a6a6d1d81a44cb72679d0d2f7
quintenvervynck <vervynck.quinten@gmail.com>
2022-12-04 14:52:58 +0100

    rm old code

--- markdir.py
+++ markdir.py
@@ -14,32 +14,6 @@ branch = '│   '
 tee =    '├── '
 last =   '└── '
 
-"""
-def tree(dir_path: Path, prefix: str=''):
-    #A recursive generator, given a directory Path object
-    #will yield a visual tree structure line by line
-    #with each line prefixed by the same characters
-
-    contents = list(sorted(filter(ignore, dir_path.iterdir())))
-    # contents each get pointers that are ├── with a final └── :
-    pointers = [tee] * (len(contents) - 1) + [last]
-    for pointer, path in zip(pointers, contents):
-        name = path.name[:-3] if path.name[-3:] == ".md" else str(path.name)
-        if 
-        yield f"[`{prefix}{pointer}{name}`]({str(path).replace(' ' , '%20')})"
-        if path.is_dir(): # extend the prefix and recurse:
-            extension = branch if pointer == tee else space 
-            # i.e. space because last, └── , above so no more |
-            yield from tree(path, prefix=prefix+extension)
-
-
-prevline = Path().absolute().name  # get the current dir name
-for line in tree(Path()):
-    print(prevline + "\\")
-    prevline = line
-print(prevline)
-"""
-
 def create_index():
     with open("index.md", "w") as f:
         f.write("## Roadmap\n")
