fafda8b1e8b60395399b01cd3d288b57fd6d2f04
quintenvervynck <vervynck.quinten@gmail.com>
2022-12-04 13:27:48 +0100

    progress

---
+++ markdir.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import os
+
+ignore_list = {".DS_Store", ".git", ".gitkeep"}
+ignore_extensions = { ".png", ".jpg", ".svg"}
+def ignore(name):
+    return name not in ignore_list and name.split(".")[-1] not in ignore_extensions
+
+# prefix components:
+space =  '  '
+branch = '│   '
+# pointers:
+tee =    '├── '
+last =   '└── '
+
+"""
+def tree(dir_path: Path, prefix: str=''):
+    #A recursive generator, given a directory Path object
+    #will yield a visual tree structure line by line
+    #with each line prefixed by the same characters
+
+    contents = list(sorted(filter(ignore, dir_path.iterdir())))
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        name = path.name[:-3] if path.name[-3:] == ".md" else str(path.name)
+        if 
+        yield f"[`{prefix}{pointer}{name}`]({str(path).replace(' ' , '%20')})"
+        if path.is_dir(): # extend the prefix and recurse:
+            extension = branch if pointer == tee else space 
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix+extension)
+
+
+prevline = Path().absolute().name  # get the current dir name
+for line in tree(Path()):
+    print(prevline + "\\")
+    prevline = line
+print(prevline)
+"""
+
+def create_index():
+    with open("roadmap.md", "w") as f:
+        f.write("# Roadmap\n\n")
+        for year in sorted(os.listdir("./roadmap"), reverse=True):
+            f.write(f"{year}\n")
+            for dir in sorted(filter(ignore, os.listdir(f"./roadmap/{year}")), reverse=True):
+                if os.path.exists(f"./roadmap/{year}/{dir}/README.md"):
+                    f.write(f"[`{dir}`]({f'./roadmap/{year}/{dir}/REAMDE.md'.replace(' ' , '%20')})\n")
+                else:
+                    f.write(f"`{dir}`\n")
+
+if __name__ == "__main__":
+    create_index()
